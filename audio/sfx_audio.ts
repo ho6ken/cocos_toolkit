9a72f699da8222000adb9c626591ea91af58df26
Kent <>
2025-07-03 14:11:41 +0800

    feat: sfx audio

--- audio/sfx_audio.ts
+++ audio/sfx_audio.ts
@@ -10,10 +10,42 @@ export class SfxAudio extends AudioSource {
     public onComplete: (src: SfxAudio) => void = null;
 
     /**
+     * 編號
+     */
+    private _pid: number = -1;
+
+    /**
+     * 編號
+     */
+    public get pid(): number { return this._pid; }
+
+    /**
      * 初始化
      * @param id 流水號
      */
     public init(id: number): void {
-        // TODO
+        this._pid = id;
+        this.node.name = id.toString();
     }
+
+    /**
+     * 播放完畢
+     */
+    private onFinish(): void {
+        this.onComplete && this.onComplete(this);
+    }
+
+    /**
+     * 
+     */
+    public onLoad(): void {
+        this.node.on(AudioSource.EventType.ENDED, this.onFinish, this);
+    }
+
+    /**
+     * 
+     */
+    public onDestroy(): void { 
+        this.node.off(AudioSource.EventType.ENDED, this.onFinish, this);
+    } 
 }
